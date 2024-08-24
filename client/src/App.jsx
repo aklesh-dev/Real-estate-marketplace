97662620985a083c9e78331489f409c1d10a047e
aklesh-dev <arzunayavadu998@gmail.com>
2024-08-24 13:03:52 +0545

    create pages and install routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
 
 function App() {
-  
-  return (
-    <>
-      <main className='text-base font-bold text-center bg-slate-50'>
-        <h1>This is a component item</h1>
-      </main>
-    </>
-  )
-}
+
+  return <>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </>
+
+};
 
 export default App;
