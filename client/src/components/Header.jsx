84d86bdf5a33bcfa22944d1bba4fcf1947c9ebc3
aklesh-dev <arzunayavadu998@gmail.com>
2024-08-24 16:09:09 +0545

    create header component

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import { IoSearch } from "react-icons/io5";
+import logo from '../assets/logo.png';
+
+export default function Header() {
+    return (
+        <header className='bg-slate-100'>
+            <nav aria-label='Main navigation'
+                className='px-5 py-3 flex flex-wrap justify-between items-center shadow-md sticky top-0'
+            >
+                <Link to='/' className='flex items-center gap-2'>
+                    <img src={logo} alt='Estate-logo' className='w-[50px] h-[50px] rounded-tl-xl rounded-br-2xl object-cover shadow-sm' />
+                    <h1 className='text-2xl font-semibold text-slate-600'>Estate</h1>
+                </Link>
+                <form aria-label='Search form' className='bg-slate-300 rounded-full p-2 flex items-center px-3' >
+                    <input type="text" placeholder='search...' name="" id=""
+                        className='bg-transparent focus:outline-none w-24 sm:w-60'
+                        aria-label='Search input'
+                    />
+                    <IoSearch aria-hidden='true' className='text-slate-600' />
+                </form>
+                <ul className='flex gap-6'>
+                    <li className='font-semibold hover:underline decoration-2 decoration-dashed decoration-slate-400 p-2 rounded-full transition-all ease-linear'>
+                        <Link to="/" aria-label='Go to home page'>Home</Link>
+                    </li>
+                    <li className='font-semibold hover:underline decoration-2 decoration-dashed decoration-slate-500  p-2 rounded-full transition-all ease-linear'>
+                        <Link to="/about" aria-label='Learn more about us'>About</Link>
+                    </li>
+                    <li className='font-semibold hover:underline decoration-2 decoration-dashed decoration-sky-400  p-2 rounded-full transition-all ease-linear'>
+                        <Link to="/sign-in" aria-label='Sign in to your account'>Sign In</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
