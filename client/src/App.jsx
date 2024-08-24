0311c2eefa9abc0dd21afd83eddec77425865ae9
aklesh-dev <arzunayavadu998@gmail.com>
2024-08-24 12:40:49 +0545

    create react template with tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  
+  return (
+    <>
+      <main className='text-base font-bold text-center bg-slate-50'>
+        <h1>This is a component item</h1>
+      </main>
+    </>
+  )
+}
+
+export default App;
