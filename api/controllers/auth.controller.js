2281227aa773cd1422c39542e1f47c09f4174d66
aklesh-dev <arzunayavadu998@gmail.com>
2024-08-25 19:39:20 +0545

    create sign-in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import bcryptjs from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 import User from "../models/user.model.js";
+import { errorHandler } from '../utils/error.js';
 
 export const signUp = async(req, res, next) => {
     const {username, email, password } = req.body; 
@@ -11,5 +13,24 @@ export const signUp = async(req, res, next) => {
         
     } catch (error) {
         next(error);     
+    }
+};
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if(!validUser) return next(errorHandler(404, "User not found!"));
+        const isValidPassword = bcryptjs.compareSync(password, validUser.password);
+        if(!isValidPassword) return next(errorHandler(401, "Invalid credentials!"));
+        const {password: hashedPassword, ...userInfo} = validUser._doc;
+        const token = jwt.sign({ id: validUser._id}, process.env.JWT_SECRET);
+        res.cookie('access_token', token, {
+            httpOnly: true,
+            expires: new Date(Date.now()+3600000),
+        }).status(201).json(userInfo);
+
+    } catch (error) {
+        next(error);        
     }
 };
