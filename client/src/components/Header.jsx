356c0ebc10a0834a3cfb63a665f2efd1a0531fa2
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-02 18:00:58 +0545

    complete header search form functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,30 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { IoSearch } from "react-icons/io5";
 import logo from '../assets/logo.png';
 import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
     const { currentUser } = useSelector((state) => state.user);
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigation = useNavigate();
 
-    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(window.location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigation(`/search?${searchQuery}`)
+    };
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if(searchTermFromUrl){
+            setSearchTerm(searchTermFromUrl);
+        };
+    },[location.search]);
+
     return (
         <header className='bg-slate-100'>
             <nav aria-label='Main navigation'
@@ -16,12 +34,16 @@ export default function Header() {
                     <img src={logo} alt='Estate-logo' className='w-[50px] h-[50px] rounded-tl-xl rounded-br-2xl object-cover shadow-sm' />
                     <h1 className='text-2xl font-semibold text-slate-600'>Estate</h1>
                 </Link>
-                <form aria-label='Search form' className='bg-slate-300 rounded-full p-2 flex items-center px-3' >
+                <form onSubmit={handleSubmit} aria-label='Search form' className='bg-slate-300 rounded-full p-2 flex items-center px-3' >
                     <input type="text" placeholder='search...' name="" id=""
                         className='bg-transparent focus:outline-none w-24 sm:w-60'
                         aria-label='Search input'
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
                     />
-                    <IoSearch aria-hidden='true' className='text-slate-600' />
+                    <button>
+                        <IoSearch aria-hidden='true' className='text-slate-600' />
+                    </button>
                 </form>
                 <ul className='flex items-center gap-6'>
                     <li className='font-semibold hover:underline decoration-2 decoration-dashed decoration-slate-400 p-2 rounded-full transition-all ease-linear'>
