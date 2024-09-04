32ba989058934efd68af4e13174bf6eeaecf89ff
aklesh-dev <arzunayavadu998@gmail.com>
2024-09-04 11:47:46 +0545

    include dist in static path and build in vite config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // Enable source maps for easier debugging
+  },
   server: {
     proxy: {
       '/api': {
